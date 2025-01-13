654c4eac74abf440badc645ba927495d617a841c
XinShou <icexinshou@gmail.com>
2025-01-14 00:15:17 +0800

    fix: try to fix deply bug

--- src/router/index.ts
+++ src/router/index.ts
@@ -9,6 +9,6 @@ export const router = createRouter({
   history: createWebHistory('ntust-work-PPST'),
   routes: [
     { path: '/', name: 'student', component: StudentPage },
-    { path: '/admin', name: 'admin', component: AdminPage }
+    { path: '/admin/', name: 'admin', component: AdminPage }
   ]
 })
