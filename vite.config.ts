f774177b8dfb01f1774c601d161f97a66a17cf25
XinShou <icexinshou@gmail.com>
2025-01-13 23:12:55 +0800

    feat: support github deply

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
-// vite.config.ts
 import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from "path";
 
 export default defineConfig({
-    base: "ntust-work-PPST",
+    base: "/ntust-work-PPST/",
     plugins: [vue()],
     resolve: {
         alias: {
