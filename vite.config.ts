2d5f2dd25efd8ca1e06045f0e723a2324bbc3fc1
xinshoutw <icexinshou@gmail.com>
2025-01-17 01:45:44 +0800

    fix: try to deply on vercel

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import vue from '@vitejs/plugin-vue'
 import path from 'path'
 
 export default defineConfig({
-    base: '/ntust-work-PPST/',
     plugins: [vue()],
     resolve: {
         alias: {
